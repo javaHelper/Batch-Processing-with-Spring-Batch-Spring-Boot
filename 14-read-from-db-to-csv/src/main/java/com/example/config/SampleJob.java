1ede80e22f9e1256147fcea7572ceab6d97bef60
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-12-06 01:22:27 +0530

    Spring Boot 3.5.8

--- 14-read-from-db-to-csv/src/main/java/com/example/config/SampleJob.java
+++ 14-read-from-db-to-csv/src/main/java/com/example/config/SampleJob.java
@@ -1,17 +1,12 @@
 package com.example.config;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.Writer;
-import java.util.Date;
-
-import javax.sql.DataSource;
-
+import com.example.model.StudentJdbc;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.file.FlatFileFooterCallback;
 import org.springframework.batch.item.file.FlatFileHeaderCallback;
@@ -23,54 +18,61 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.transaction.PlatformTransactionManager;
 
-import com.example.model.StudentJdbc;
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Date;
 
 @Configuration
 public class SampleJob {
 
 	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
+	private JobRepository jobRepository;
 
 	@Autowired
-	private StepBuilderFactory stepBuilderFactory;
-	
-	@Autowired
 	private DataSource dataSource;
+
+	@Autowired
+	private PlatformTransactionManager transactionManager;
 	
 	@Bean
 	public Job chunkJob() {
-		return jobBuilderFactory.get("Chunk Job")
+		return new JobBuilder("Chunk Job", jobRepository)
 				.incrementer(new RunIdIncrementer())
 				.start(firstChunkStep())
 				.build();
 	}
-	
-	private Step firstChunkStep() {
-		return stepBuilderFactory.get("First Chunk Step")
-				.<StudentJdbc, StudentJdbc>chunk(100)
+
+    @Bean
+	public Step firstChunkStep() {
+		return new StepBuilder("First Chunk Step", jobRepository)
+				.<StudentJdbc, StudentJdbc>chunk(100, transactionManager)
 				.reader(jdbcCursorItemReader())
 				.writer(flatFileItemWriter())
 				.build();
 	}
-	
+
+    @Bean
 	public JdbcCursorItemReader<StudentJdbc> jdbcCursorItemReader() {
-		JdbcCursorItemReader<StudentJdbc> jdbcCursorItemReader = new JdbcCursorItemReader<StudentJdbc>();
+		JdbcCursorItemReader<StudentJdbc> jdbcCursorItemReader = new JdbcCursorItemReader<>();
 		jdbcCursorItemReader.setDataSource(dataSource);
 		jdbcCursorItemReader.setSql("select id, first_name as firstName, last_name as lastName,email from students");
 
-		jdbcCursorItemReader.setRowMapper(new BeanPropertyRowMapper<StudentJdbc>() {
-			{
-				setMappedClass(StudentJdbc.class);
-			}
-		});
+		jdbcCursorItemReader.setRowMapper(new BeanPropertyRowMapper<>() {
+            {
+                setMappedClass(StudentJdbc.class);
+            }
+        });
 		return jdbcCursorItemReader;
 	}
 	
 	@Bean
 	public FlatFileItemWriter<StudentJdbc> flatFileItemWriter() {
 		FlatFileItemWriter<StudentJdbc> flatFileItemWriter = new FlatFileItemWriter<StudentJdbc>();
-		flatFileItemWriter.setResource(new FileSystemResource(new File("outputFiles/students.csv")));
+		flatFileItemWriter.setResource(new FileSystemResource(new File("./outputFiles/students.csv")));
 		
 		flatFileItemWriter.setHeaderCallback(new FlatFileHeaderCallback() {
 			@Override
@@ -79,15 +81,15 @@ public class SampleJob {
 			}
 		});
 		
-		flatFileItemWriter.setLineAggregator(new DelimitedLineAggregator<StudentJdbc>() {
-			{
-				setFieldExtractor(new BeanWrapperFieldExtractor<StudentJdbc>() {
-					{
-						setNames(new String[] {"id", "firstName", "lastName", "email"});
-					}
-				});
-			}
-		});
+		flatFileItemWriter.setLineAggregator(new DelimitedLineAggregator<>() {
+            {
+                setFieldExtractor(new BeanWrapperFieldExtractor<>() {
+                    {
+                        setNames(new String[]{"id", "firstName", "lastName", "email"});
+                    }
+                });
+            }
+        });
 		
 		flatFileItemWriter.setFooterCallback(new FlatFileFooterCallback() {
 			@Override
