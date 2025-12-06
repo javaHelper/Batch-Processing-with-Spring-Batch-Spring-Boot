16c69ee3b698ac8305295bc072af8cfe58ead54e
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-12-06 12:57:07 +0530

    Spring Boot 3.5.8

--- 22-capture-bad-record-while-writting/src/main/java/com/example/reader/FirstItemReader.java
+++ 22-capture-bad-record-while-writting/src/main/java/com/example/reader/FirstItemReader.java
@@ -11,21 +11,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class FirstItemReader implements ItemReader<Integer> {
-	
-	List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-	int i = 0;
 
-	@Override
-	public Integer read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-		System.out.println("Inside Item Reader");
-		Integer item;
-		if(i < list.size()) {
-			item = list.get(i);
-			i++;
-			return item;
-		}
-		i = 0;
-		return null;
-	}
+    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    int i = 0;
 
+    @Override
+    public Integer read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+        System.out.println("Inside Item Reader");
+        Integer item;
+        if (i < list.size()) {
+            item = list.get(i);
+            i++;
+            return item;
+        }
+        i = 0;
+        return null;
+    }
 }
